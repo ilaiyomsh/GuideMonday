8e33c11a8b4ef72bb3ab1c7674f5d19a24aa46c9
ilaiyomsh <ilai@twyst.co.il>
2025-09-28 05:14:56 +0300

    תיקון פלטת הצבעים וכיוון הכתב
    - תיקון פלטת הצבעים - עכשיו נפתחת בלחיצה על כפתור הצבע
    - הוספת event listener לסגירת palette בלחיצה מחוץ לו
    - שיפור RTL - הוספת writing-mode: horizontal-tb
    - הוספת direction: rtl לכלל הדיאלוג
    - וידוא שהעורך עובד נכון עם עברית מימין לשמאל

--- src/components/ContentBlockEditDialog.jsx
+++ src/components/ContentBlockEditDialog.jsx
@@ -132,6 +132,18 @@ export default function ContentBlockEditDialog({
     }
   };
 
+  // Close color palette when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (showColorPalette && !event.target.closest('.color-picker-container')) {
+        setShowColorPalette(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, [showColorPalette]);
+
   const renderFormFields = () => {
     switch (block.type) {
       case 'text':
@@ -256,28 +268,26 @@ export default function ContentBlockEditDialog({
                         <path d="M9.62,12L12,5.67L14.37,12M11,3L5.5,17H7.5L8.62,14H15.37L16.5,17H18.5L13,3H11M21,19V22H3V19H21Z"></path>
                       </svg>
                     </button>
-                    {showColorPalette && (
-                      <div className="color-palette">
-                        {[
-                          '#000000', '#434343', '#666666', '#999999', '#b7b7b7', '#cccccc', '#d9d9d9', '#efefef', '#f3f3f3', '#ffffff',
-                          '#980000', '#ff0000', '#ff9900', '#ffff00', '#00ff00', '#00ffff', '#4a86e8', '#0000ff', '#9900ff', '#ff00ff',
-                          '#e6b8af', '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#c9daf8', '#cfe2f3', '#d9d2e9', '#ead1dc',
-                          '#dd7e6b', '#ea9999', '#f9cb9c', '#ffe599', '#b6d7a8', '#a2c4c9', '#a4c2f4', '#9fc5e8', '#b4a7d6', '#d5a6bd',
-                          '#d0442f', '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6d9eeb', '#6fa8dc', '#8e7cc3', '#c27ba0',
-                          '#a61c00', '#cc0000', '#e69138', '#f1c232', '#6aa84f', '#45818e', '#3c78d8', '#3d85c6', '#674ea7', '#a64d79',
-                          '#85200c', '#990000', '#b45f06', '#bf9000', '#38761d', '#134f5c', '#0b5394', '#351c75', '#741b47',
-                          '#5b0f00', '#660000', '#783f04', '#7f6000', '#274e13', '#0c343d', '#073763', '#20124d', '#4c1130'
-                        ].map(color => (
-                          <div
-                            key={color}
-                            className="color-swatch"
-                            style={{ backgroundColor: color }}
-                            onClick={() => selectColor(color)}
-                            title={`צבע ${color}`}
-                          />
-                        ))}
-                      </div>
-                    )}
+                    <div className={`color-palette ${showColorPalette ? 'show' : ''}`}>
+                      {[
+                        '#000000', '#434343', '#666666', '#999999', '#b7b7b7', '#cccccc', '#d9d9d9', '#efefef', '#f3f3f3', '#ffffff',
+                        '#980000', '#ff0000', '#ff9900', '#ffff00', '#00ff00', '#00ffff', '#4a86e8', '#0000ff', '#9900ff', '#ff00ff',
+                        '#e6b8af', '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#c9daf8', '#cfe2f3', '#d9d2e9', '#ead1dc',
+                        '#dd7e6b', '#ea9999', '#f9cb9c', '#ffe599', '#b6d7a8', '#a2c4c9', '#a4c2f4', '#9fc5e8', '#b4a7d6', '#d5a6bd',
+                        '#d0442f', '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6d9eeb', '#6fa8dc', '#8e7cc3', '#c27ba0',
+                        '#a61c00', '#cc0000', '#e69138', '#f1c232', '#6aa84f', '#45818e', '#3c78d8', '#3d85c6', '#674ea7', '#a64d79',
+                        '#85200c', '#990000', '#b45f06', '#bf9000', '#38761d', '#134f5c', '#0b5394', '#351c75', '#741b47',
+                        '#5b0f00', '#660000', '#783f04', '#7f6000', '#274e13', '#0c343d', '#073763', '#20124d', '#4c1130'
+                      ].map(color => (
+                        <div
+                          key={color}
+                          className="color-swatch"
+                          style={{ backgroundColor: color }}
+                          onClick={() => selectColor(color)}
+                          title={`צבע ${color}`}
+                        />
+                      ))}
+                    </div>
                   </div>
                 </div>
                 
